4e91014b1e5b11caf22b1c09cd35fb0d66db82e0
Miguel <diaz4674@gmail.com>
2019-05-21 21:36:48 -0700

    Completed MVP

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const server = express();
 
+const accountsRouter = require('./data/accounts-modelRouter')
 // your code here
+
+server.use(express.json())
+
+//Routes
+server.use('/api/accountsModel', accountsRouter)
+
+server.get('/', (req, res) =>{
+    res.send('Welcome to the accounts!')
+})
 
 module.exports = server;
